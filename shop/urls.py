15b01ac9d60cf4cfe2a84afcc69f974907cd283d
vladisa88 <vladisa88@gmail.com>
2020-06-19 14:32:46 +0400

    initial, sessions, cart, orders

---
+++ shop/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'shop'
+
+urlpatterns = [
+    path('', views.ProductList.as_view(), name='product_list'),
+    path('<slug:category_slug>/', views.ProductList.as_view(),
+         name='product_list_by_category'),
+    path('<int:id>/<slug:slug>/', views.ProductDetail.as_view(),
+         name='product_detail')
+]
---
+++ shop/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+
+from .models import Category, Product
+from cart.forms import CartAddProductForm
+
+
+class ProductList(generic.View):
+    def get(self, request, category_slug=None):
+        category = None
+        categories = Category.objects.all()
+        products = Product.objects.filter(available=True)
+        if category_slug:
+            category = get_object_or_404(Category, slug=category_slug)
+            products = products.filter(category=category)
+        return render(request, 'shop/product/list.html',
+                      {'category': category,
+                       'categories': categories,
+                       'products': products})
+
+
+class ProductDetail(generic.View):
+    def get(self, request, id, slug):
+        product = get_object_or_404(Product, id=id, slug=slug,
+                                    available=True)
+        cart_product_form = CartAddProductForm()
+        return render(request, 'shop/product/detail.html',
+                      {'product': product,
+                       'cart_product_form': cart_product_form})
